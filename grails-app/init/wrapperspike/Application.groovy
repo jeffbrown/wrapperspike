94f1c5299283f4beb82068815d14f20370af1346
Jeff Scott Brown <jbrown@pivotal.io>
2015-03-23 15:01:18 -0500

    grailsw experiement

---
+++ grails-app/init/wrapperspike/Application.groovy
@@ -0,0 +1,10 @@
+package wrapperspike
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application)
+    }
+}
